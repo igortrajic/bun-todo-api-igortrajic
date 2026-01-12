5326da69ef4c42605adc62d4c306eadf6575d7dd
igortrajic <iggetrajic@gmail.com>
2026-01-12 10:34:02 +0100

    feat: added patch method closes #5

--- index.ts
+++ index.ts
@@ -9,9 +9,24 @@ const todoSchema = v.object({
     v.transform(s => s.trim()),
     v.minLength(1, "Title is required")
   ),
-  description: v.optional(v.string()),
-  date: v.optional(v.string())
+  content: v.optional(v.string()),
+  date: v.optional(v.pipe(v.string(), v.isoDate()))
 });
+
+const updateTodoSchema = v.object({
+  title: v.optional(
+  v.pipe(
+    v.string(),
+    v.transform(s => s.trim()),
+    v.minLength(1, "Title is required")
+  ),
+),
+  content: v.optional(v.string()),
+  date: v.optional(v.pipe(v.string(), v.isoDate())),
+  done: v.optional(v.boolean()),
+  id: v.number() 
+});
+
 const server = Bun.serve({
   port: 3000,
   routes: {
@@ -31,19 +46,58 @@ const server = Bun.serve({
           const body = await req.json();
           const todo = v.parse(todoSchema, body);
 
-          db.query(
+          const info = db.query(
             "INSERT INTO todos (title, content, due_date) VALUES (?, ?, ?)"
           ).run(
             todo.title,
-            todo.description ?? null,
+            todo.content ?? null,
             todo.date ?? null
           );
 
-          return Response.json({ success: true, todo }, { status: 201 });
+          return Response.json({ 
+            success: true, 
+            todo: { ...todo, id: info.lastInsertRowid } 
+          }, { status: 201 });
         } catch (err) {
           if (err instanceof v.ValiError) {
             return Response.json(
-              { success: false, error: "Invalid todo" },
+              { success: false, error: "Invalid todo", issues: err.issues },
+              { status: 400 }
+            );
+          }
+
+          console.error(err);
+          return Response.json(
+            { success: false, error: "Server error" },
+            { status: 500 }
+          );
+        }
+      },
+      PATCH: async (req) => {
+        try {
+          const body = await req.json();
+          const todo = v.parse(updateTodoSchema, body);
+
+          const info = db.query(
+            "UPDATE todos set title = COALESCE(?,title),content = COALESCE(?,content), due_date = COALESCE(?,due_date), done = COALESCE(?,done) WHERE id = ?"
+          ).run(
+            todo.title ?? null,
+            todo.content ?? null,
+            todo.date ?? null,
+            todo.done === undefined ? null : (todo.done ? 1 : 0),
+            todo.id
+          );
+          if (info.changes === 0) {
+            return Response.json(
+              { success: false, error: "Todo not found" }, 
+              { status: 404 }
+            );
+          }
+          return Response.json({ success: true, todo }, { status: 200 });
+        } catch (err) {
+          if (err instanceof v.ValiError) {
+            return Response.json(
+              { success: false, error: "Invalid todo", issues: err.issues },
               { status: 400 }
             );
           }
